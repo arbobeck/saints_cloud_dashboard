d4cbbf6abb4d1831220e9860954013504def8cf8
Adam <arbobeck@gmail.com>
2025-12-21 13:57:37 +0100

    update routes

--- ui/saints-ui/src/app/app.routes.ts
+++ ui/saints-ui/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Liturgy } from './pages/liturgy/liturgy';
@@ -8,14 +7,12 @@ import { Resources } from './pages/resources/resources';
 import { Impressum } from './pages/impressum/impressum';
 import { Datenschutz } from './pages/datenschutz/datenschutz';
 import { Uptime } from './pages/uptime/uptime';
-
 import { LoginComponent } from './components/login/login';
 import { AdminComponent } from './components/admin/admin';
 import { EditorComponent } from './components/editor/editor';
 import { authGuard } from './guards/auth.guard';
 import { BlogListComponent } from './blog/blog-list/blog-list.component';
 import { BlogPostComponent } from './blog/blog-post/blog-post.component';
-
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -25,15 +22,21 @@ export const routes: Routes = [
   { path: 'impressum', component: Impressum },
   { path: 'datenschutz', component: Datenschutz },
   { path: 'uptime', component: Uptime },
-  { path: 'admin/login', component: LoginComponent },
-  { path: 'admin/', component: AdminComponent, canActivate: [authGuard] },
-  { path: 'admin/editor', component: EditorComponent, canActivate: [authGuard] },
-  { path: 'admin/editor/:id', component: EditorComponent, canActivate: [authGuard] },
+  
+  // Blog routes
   { path: 'blog', component: BlogListComponent },
   { path: 'blog/:slug', component: BlogPostComponent },
+  
+  // Admin routes
+  { path: 'admin/login', component: LoginComponent },
+  { path: 'admin/dashboard', component: AdminComponent, canActivate: [authGuard] },
+  { path: 'admin/editor', component: EditorComponent, canActivate: [authGuard] },
+  { path: 'admin/editor/:id', component: EditorComponent, canActivate: [authGuard] },
+  { path: 'admin', redirectTo: 'admin/login', pathMatch: 'full' },
+  
+  // Catch-all
   { path: '**', redirectTo: '' }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
